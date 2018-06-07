3288b47d2679bcf8fb8b0b40de46ec5339d66c9f
William Chong <williamchong007@msn.com>
2018-06-07 22:38:23 +0800

    + Include nuxt config in firebase deploy flow

--- functions/ssrapp.js
+++ functions/ssrapp.js
@@ -1,7 +1,10 @@
 const functions = require('firebase-functions');
 const { Nuxt } = require('nuxt-edge');
 
+const nuxtConfig = require('./nuxt.config.js');
+
 const config = {
+  ...nuxtConfig,
   dev: false,
   debug: true,
   buildDir: 'nuxt',
