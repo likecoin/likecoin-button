3dfd728642ba4250e2fe8be5b77bc4e903b7fe1d
William Chong <williamchong007@msn.com>
2018-06-08 16:41:48 +0800

    + Add id-done page

--- src/pages/_id/done.vue
+++ src/pages/_id/done.vue
@@ -1,21 +1,11 @@
 <template>
   <section class="container">
-    <vue-recaptcha
-      ref="invisibleRecaptcha"
-      size="invisible"
-      @verify="onCaptchaVerify"
-      @expired="onCaptchaExpired"
-      @render="onCaptchaRender"
-      sitekey="6Le9w10UAAAAANsMwDA5YuiwCudW8YKu2RGI8Hcl" />
     <div>
       <h1 class="title">
         likecoin-button
       </h1>
       <img v-if="avatar" :src="avatar" />
-      <h2 v-if="isError">{{ isNotFound ? `User ${likee} Not Found` : 'Error' }}</h2>
-      <h2 v-else-if="isNeedCaptcha">Checking reCaptcha...</h2>
-      <h2 v-else-if="!isDone">Loading...</h2>
-      <h2 v-else class="subtitle">
+      <h2 class="subtitle">
         You have just liked {{ likee }}
       </h2>
       <div class="links">
@@ -28,21 +18,10 @@
 </template>
 
 <script>
-import VueRecaptcha from 'vue-recaptcha';
 import axios from '~/plugins/axios';
 
 export default {
   name: 'id',
-  components: { VueRecaptcha },
-  data() {
-    return {
-      reCaptchaResponse: '',
-      isNeedCaptcha: false,
-      isDone: false,
-      isError: false,
-      isNotFound: false,
-    };
-  },
   computed: {
     likee() {
       return this.$route.params.id;
@@ -96,36 +75,6 @@ export default {
         },
       ],
     };
-  },
-  methods: {
-    async postLike() {
-      try {
-        await axios.post(
-          `/api/like/${this.likee}`,
-          { reCaptchaResponse: this.reCaptchaResponse },
-          { headers: { 'Like-Referer': document.referrer } },
-        );
-      } catch (err) {
-        if (err.response && err.response.status === 404) {
-          this.isNotFound = true;
-        }
-        this.isError = true;
-      }
-      this.isDone = true;
-      this.$router.replace({ name: 'id-done', params: this.$route.params });
-    },
-    onCaptchaVerify(response) {
-      this.isNeedCaptcha = false;
-      this.reCaptchaResponse = response;
-      if (!this.isDone) this.postLike();
-    },
-    onCaptchaExpired() {
-      this.reCaptchaResponse = '';
-    },
-    onCaptchaRender() {
-      this.isNeedCaptcha = true;
-      this.$refs.invisibleRecaptcha.execute();
-    },
   },
 };
 </script>
