dc607114ddc09cd9fb62936a455f12e991b795bb
Simonb the game designer <simonbut711@gmail.com>
2018-06-26 12:32:57 +0800

    Update index.js

--- src/locales/index.js
+++ src/locales/index.js
@@ -1,19 +1,8 @@
 import en from './client/en.json';
-import zh from './client/zh.json';
-// import cn from './cn.json';
-// import ja from './ja.json';
-// import ko from './ko.json';
-// import de from './de.json';
-// import es from './es.json';
-// import fr from './fr.json';
-// import it from './it.json';
-// import pt from './pt.json';
-// import ru from './ru.json';
 
 export const defaultLocale = 'en';
 export const preloadMessages = {
   en,
-  zh,
 };
 
 export const supportedLocales = [
