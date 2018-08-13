5ee7741aa55a784199b3b94b41ff434aba51ad97
Miles Wong <miles@like.co>
2018-08-13 15:58:43 +0800

    + Add mini version of like button

---
+++ src/pages/in/embed/_id/mini.vue
@@ -0,0 +1,106 @@
+<template>
+  <like-button
+    :like-count="likeCount"
+    :total-like="totalLike"
+    :is-super-like="shouldShowBackside"
+    @like="onClickLike"
+    @super-like="onClickSuperLike"
+    @click-stats="onClickLikeStats"
+  />
+</template>
+
+<script>
+import {
+  apiPostLikeButton,
+  apiGetLikeButtonMyStatus,
+  apiGetLikeButtonTotalCount,
+} from '@/util/api/api';
+
+import mixin from '~/components/embed/mixin';
+import LikeButton from '~/components/LikeButton';
+
+const debounce = require('lodash.debounce');
+
+const debouncedOnClick = debounce((that) => {
+  /* eslint-disable no-param-reassign */
+  const count = that.likeCount - that.likeSent;
+  that.likeSent += count;
+  if (count > 0) apiPostLikeButton(that.id, that.referrer, count);
+  that.totalLike += count;
+  /* eslint-enable no-param-reassign */
+}, 500);
+
+export default {
+  name: 'embed-id-mini',
+  layout: 'embed',
+  components: {
+    LikeButton,
+  },
+  mixins: [mixin],
+  data() {
+    return {
+      isLoggedIn: false,
+      likeCount: 0,
+      likeSent: 0,
+      totalLike: 0,
+      shouldShowBackside: false,
+    };
+  },
+  computed: {
+    referrer() {
+      return this.$route.query.referrer || document.referrer || '';
+    },
+    isSuperLike() {
+      return (this.likeCount >= 5);
+    },
+  },
+  mounted() {
+    this.updateUser();
+  },
+  methods: {
+    async updateUser() {
+      try {
+        const [{ data: myData }, { data: totalData }] = await Promise.all([
+          apiGetLikeButtonMyStatus(this.id, this.referrer),
+          apiGetLikeButtonTotalCount(this.id, this.referrer),
+        ]);
+        const { liker, count } = myData;
+        const { total } = totalData;
+        this.isLoggedIn = !!liker;
+        this.totalLike = total;
+        this.likeCount = count;
+        this.likeSent = count;
+      } catch (err) {
+        console.error(err); // eslint-disable-line no-console
+      }
+    },
+    onClickLike() {
+      if (this.isSuperLike) {
+        this.shouldShowBackside = true;
+      } else {
+        this.likeCount += 1;
+      }
+      debouncedOnClick(this);
+    },
+    onClickSuperLike(e, isSuperLike) {
+      if (
+        isSuperLike &&
+        this.shouldShowBackside &&
+        this.$refs.superLikeButton
+      ) {
+        this.$refs.superLikeButton.click(e);
+      }
+      this.shouldShowBackside = isSuperLike;
+    },
+    onClickLikeStats() {
+      const { id } = this.$route.params;
+      const { referrer } = this.$route.query;
+      window.open(
+        `/in/embed/${id}/list${referrer ? `?referrer=${referrer}` : ''}`,
+        '_blank',
+        'menubar=no,location=no,width=576,height=768',
+      );
+    },
+  },
+};
+</script>
