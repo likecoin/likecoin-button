d60cf680e1bb797c430997150e5aa777d6f8219c
William Chong <williamchong007@msn.com>
2019-01-16 14:17:14 +0800

    + Polyfill Math.cbrt

--- src/plugins/polyfill.js
+++ src/plugins/polyfill.js
@@ -2,4 +2,5 @@ require('core-js/fn/object/assign');
 require('core-js/fn/object/values');
 require('core-js/fn/string/includes');
 require('core-js/fn/array/includes');
+require('core-js/fn/math/cbrt');
 require('classlist-polyfill');
