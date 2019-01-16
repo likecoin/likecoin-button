97705338335b67d4c3e80c16a07fa2ec706b6cab
William Chong <williamchong007@msn.com>
2019-01-16 14:46:00 +0800

    + Add timeout in axios

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,6 +1,9 @@
 import * as axios from 'axios';
 
-const options = { withCredentials: true };
+const options = {
+  withCredentials: true,
+  timeout: 5000,
+};
 // The server-side needs a full url to works
 // if (process.server) {
 //   options.baseURL = `http://${process.env.HOST || 'localhost'}:${process.env.PORT || 3000}`;
