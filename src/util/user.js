749e7bfb1a639c9abe05207252084fe70d959404
Ng Wing Tat, David <i@ngwingt.at>
2019-01-28 16:36:12 +0800

    * Update Civic Liker halo for trial users

--- src/util/user.js
+++ src/util/user.js
@@ -1,11 +1,9 @@
-import { CIVIC_LIKER_TRIAL_END_DATE } from '../constant';
-
 export function getAvatarHaloTypeFromUser(user = {}) {
+  if (user.isCivicLikerTrial) {
+    return 'civic-liker-trial';
+  }
   if (user.isSubscribedCivicLiker) {
     return 'civic-liker';
-  }
-  if (user.isPreRegCivicLiker && Date.now() <= CIVIC_LIKER_TRIAL_END_DATE) {
-    return 'civic-liker-trial';
   }
   return 'none';
 }
