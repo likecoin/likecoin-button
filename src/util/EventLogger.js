5940487b7e8adc429a6e98d384717ea150108314
William Chong <williamchong007@msn.com>
2019-02-12 18:56:29 +0800

    ✨ Respect DNT header

--- src/util/EventLogger.js
+++ src/util/EventLogger.js
@@ -5,15 +5,22 @@ export function logTrackerEvent(
   label,
   value,
 ) {
-  window.dataLayer = window.dataLayer || [];
-  window.dataLayer.push({
-    event: 'customEvent',
-    category,
-    action,
-    label,
-    value,
-  });
-  if (window.fbq) window.fbq('track', action);
+  try {
+    // do not track
+    if (window.doNotTrack || navigator.doNotTrack) return;
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+      event: 'customEvent',
+      category,
+      action,
+      label,
+      value,
+    });
+    if (window.fbq) window.fbq('track', action);
+  } catch (err) {
+    console.error('logging error:');
+    console.error(err);
+  }
 }
 
 export default logTrackerEvent;
