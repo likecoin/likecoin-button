f43b0391adc481dc10893eb6416f3aa995f5ef47
Michael Cheung <mc.kingho@gmail.com>
2019-02-22 19:52:34 +0800

    ! Fix the referrer is host url if there is no referrer in query string

--- src/pages/_id/index.vue
+++ src/pages/_id/index.vue
@@ -182,7 +182,9 @@ export default {
     urlReferrer() {
       const { query } = this.$route;
       let { referrer = '' } = query;
-      referrer = handleQueryStringInUrl(referrer);
+      if (referrer) {
+        referrer = handleQueryStringInUrl(referrer);
+      }
       return referrer;
     },
     referrer() {
