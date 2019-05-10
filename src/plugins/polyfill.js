faaf3463ca99deec1e44ed63ede5eddff8c2a24a
William Chong <williamchong007@msn.com>
2019-05-10 21:44:56 +0800

    🚑 Try to fix `isInteger` missing error

--- src/plugins/polyfill.js
+++ src/plugins/polyfill.js
@@ -3,4 +3,6 @@ require('core-js/fn/object/values');
 require('core-js/fn/string/includes');
 require('core-js/fn/array/includes');
 require('core-js/fn/math/cbrt');
+require('core-js/fn/number/is-integer');
+require('core-js/fn/number/is-nan');
 require('classlist-polyfill');
