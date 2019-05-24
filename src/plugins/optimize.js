186f0374b3e9225f516bdf84165e0e34b3e6867e
William Chong <williamchong007@msn.com>
2019-05-24 15:16:21 +0800

    💩 HACK to remove old cookie

---
+++ src/plugins/optimize.js
@@ -0,0 +1,25 @@
+/* Sync cookie __session to exp in client due to firebase fn vs GTM limitation */
+/* GTM only recognize exp as optimize cookie, while firebase fn only
+   accept __session for cookie  */
+import * as cookie from 'tiny-cookie';
+
+export default async ({ res }) => {
+  try {
+    if (process.server && res.clearCookie) {
+      /* clear legacy cookie affecting a/b testing */
+      res.clearCookie('__session',
+        {
+          path: '/',
+          domain: '.like.co',
+          secure: true,
+          httpOnly: true,
+        });
+    } else {
+      if (!document.cookie || !cookie.enabled()) return;
+      const expId = cookie.get('__session');
+      if (expId) cookie.set('exp', expId, Date.now());
+    }
+  } catch (err) {
+    console.error(err); // eslint-disable-line no-console
+  }
+};
