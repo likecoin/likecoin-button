4932df83ec1eeef30f0258d6d083beed9490b2a9
William Chong <williamchong007@msn.com>
2020-05-21 23:19:03 +0800

    ✨ Add superlike apis

--- src/util/api/api.js
+++ src/util/api/api.js
@@ -5,6 +5,8 @@ export const apiGetUserMinById = id => axios.get(`${LIKECOIN_API}/api/users/id/$
 
 export const apiGetSocialListById = (id, type = '') => axios.get(`${LIKECOIN_API}/api/social/list/${id}?type=${type}`);
 
+export const apiGetSuperLikeInfo = id => axios.get(`${LIKECOIN_MISC_API_BASE}/api/like/share/${id}`);
+
 export const apiGetLikeButtonMyStatus = (id, referrer, isCookieSupport, documentReferrer = '', sessionID = '') => {
   const cookieParam = isCookieSupport !== undefined ? `&cookie_support=${isCookieSupport ? 1 : 0}` : '';
   return axios.get(
@@ -21,6 +23,11 @@ export const apiGetLikeButtonMyStatus = (id, referrer, isCookieSupport, document
 
 export const apiGetLikeButtonSelfCount = (id, referrer) => axios.get(
   `${LIKECOIN_MISC_API_BASE}/api/like/likebutton/${id}/self/like?referrer=${encodeURIComponent(referrer)}`,
+  { withCredentials: true },
+);
+
+export const apiGetSuperLikeMyStatus = (tz = '', referrer = '') => axios.get(
+  `${LIKECOIN_MISC_API_BASE}/api/like/share/self?tz=${tz}&referrer=${encodeURIComponent(referrer)}`,
   { withCredentials: true },
 );
 
@@ -68,6 +75,22 @@ export const apiPostLikeButtonReadEvent = (id, referrer, isCookieSupport, docume
     },
   );
 };
+
+export const apiPostSuperLike = (id, referrer, tz, parentSuperLikeID, documentReferrer = '', sessionID = '') => axios.post(
+  `${LIKECOIN_MISC_API_BASE}/api/like/share/${id}`,
+  {
+    referrer,
+    tz,
+    parentSuperLikeID,
+  },
+  {
+    headers: {
+      'Document-Referrer': documentReferrer,
+      'X-Likecoin-Session-ID': sessionID,
+    },
+    withCredentials: true,
+  },
+);
 
 export const apiQueryCoinGeckoInfo = () => axios.get('https://api.coingecko.com/api/v3/coins/likecoin?localization=false', { withCredentials: false });
 
