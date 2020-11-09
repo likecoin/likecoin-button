067cf3b7733afe11f6ecaa80a70366815767946d
Ng Wing Tat, David <i@ngwingt.at>
2020-11-09 16:38:03 +0800

    ⬆️ Upgrade Storybook to v6

--- src/.storybook/main.js
+++ src/.storybook/main.js
@@ -1,4 +1,24 @@
+const path = require("path");
+
 module.exports = {
-  stories: ['../components/**/*.stories.js'],
-  addons: ['@storybook/addon-knobs'],
-};
+  "stories": [
+    "../**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-knobs",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ],
+  "webpackFinal": (config) => {
+    /**
+     * core-js compatibility issue
+     * https://github.com/storybookjs/storybook/issues/11255
+     */
+    config.resolve.alias['core-js/modules'] = path.resolve(
+      __dirname,
+      '..',
+      'node_modules/@storybook/core/node_modules/core-js/modules'
+    );
+    return config;
+  }
+}
