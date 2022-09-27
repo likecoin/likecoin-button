bb06467edb6c55e5374d3ea55c928ff598f6e67d
William Chong <me@williamchong.cloud>
2022-09-27 17:52:40 +0800

    ⬆️ Use firebase fn v2

--- functions/ssrapp.js
+++ functions/ssrapp.js
@@ -1,17 +1,11 @@
-const functions = require('firebase-functions');
+// TODO: eslint import cannot handle firebase module
+// eslint-disable-next-line import/no-unresolved
+const { onRequest } = require('firebase-functions/v2/https');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 const onHeaders = require('on-headers');
 const { Nuxt } = require('nuxt-start');
-
-if ((functions.config().likeco || {}).testmode) {
-  process.env.IS_TESTNET = true;
-}
-
-if ((functions.config().sentry || {}).report_uri) {
-  process.env.SENTRY_REPORT_URI = functions.config().sentry.report_uri;
-}
 
 const nuxtConfig = require('./nuxt.config.js');
 
@@ -48,4 +42,5 @@ app.use(async (req, res) => {
   await nuxt.ready();
   nuxt.render(req, res);
 });
-module.exports = functions.https.onRequest(app);
+
+module.exports = onRequest({ region: ['us-west1'] }, app);
