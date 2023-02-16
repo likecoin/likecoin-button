fa97a3274754c5da0b84d62ffef2be59a10492f0
William Chong <me@williamchong.cloud>
2023-02-16 16:16:35 +0800

    🌐 Refactor i18n detection and support language qs

--- src/plugins/vue-i18n.js
+++ src/plugins/vue-i18n.js
@@ -30,29 +30,34 @@ async function loadLanguageAsync(i18n, lang) {
 export default async ({
   app,
   req,
+  query,
 }) => {
   // Set i18n instance on app to use it in middleware and pages asyncData/fetch
-  let locale = defaultLocale;
-  if (!process.server) {
-    let navLangs = [];
-    if (navigator.language) navLangs.push(navigator.language);
-    if (navigator.languages && navigator.languages.length) {
-      navLangs = navLangs.concat(navigator.languages);
-    }
-    supportedLocales.find((supportedLocale) => {
-      const match = navLangs.find(navLang => navLang.toLowerCase().includes(supportedLocale));
-      if (match) {
-        locale = supportedLocale;
+  let locale = query.language;
+  if (!locale) {
+    if (!process.server) {
+      let navLangs = [];
+      if (navigator.language) navLangs.push(navigator.language);
+      if (navigator.languages && navigator.languages.length) {
+        navLangs = navLangs.concat(navigator.languages);
       }
-      return !!match;
-    });
-  } else if (req) {
-    locale = (
-      (req.acceptsLanguages && req.acceptsLanguages(supportedLocales)) ||
-      defaultLocale
-    );
-    if (!supportedLocales.includes(locale)) locale = defaultLocale;
+      for (let i = 0; i < navLangs.length; i += 1) {
+        const navLang = navLangs[i];
+        const match = supportedLocales
+          .find(supportedLocale => navLang.toLowerCase().includes(supportedLocale));
+        if (match) {
+          locale = match;
+          break;
+        }
+      }
+    } else if (req) {
+      locale = (
+        (req.acceptsLanguages && req.acceptsLanguages(supportedLocales)) ||
+        defaultLocale
+      );
+    }
   }
+  if (!supportedLocales.includes(locale)) locale = defaultLocale;
   app.i18n = new VueI18n({
     locale,
     fallbackLocale: defaultLocale,
