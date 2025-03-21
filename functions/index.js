5fc9bf4914f52b0da61f8803dd61955a3247e800
William Chong <me@williamchong.cloud>
2025-03-22 02:39:43 +0800

    🔥 Remove superlike

--- functions/index.js
+++ functions/index.js
@@ -4,10 +4,6 @@ if (!process.env.K_SERVICE || process.env.K_SERVICE === 'ssrapp') {
   exports.ssrapp = require('./ssrapp');
 }
 
-if (!process.env.K_SERVICE || process.env.K_SERVICE === 'superlike') {
-  exports.superlike = require('./superlike');
-}
-
 if (!process.env.K_SERVICE || process.env.K_SERVICE === 'imagessr') {
   exports.imagessr = require('./imagessr');
 }
